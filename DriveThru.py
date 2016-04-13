4c7d93eb8f3452761ef6bca67d7eb6b6ffa22673
DRaines234 <DRaines574@gmail.com>
2016-04-13 00:38:20 -0400

    -Created "main" and called it DriveThru so we'd have a python file

---
+++ DriveThru.py
@@ -0,0 +1,8 @@
+__author__ = 'Derek and Spencer'
+
+
+def main():
+    print("I want a double cheeseburger and hold the lettuce, don't be frontin' son, no seeds on the bun.")
+
+if __name__ == "__main__":
+    main()
