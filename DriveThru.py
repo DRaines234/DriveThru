9245fd363692b6f8a5638f10ae723a5568553022
Spencer Diernbach <stsld05@moravian.edu>
2016-04-17 22:28:40 -0400

    started making classes and such

--- DriveThru.py
+++ DriveThru.py
@@ -1,5 +1,19 @@
 __author__ = 'Derek and Spencer'
 
+import OrderQueue
+import rngs
+
+def run_sim(orderQueueSize, payQueueSize, pickupQueueSize):
+    orderQSize = orderQueueSize
+    payQSize = payQueueSize
+    pickupQSize = pickupQueueSize
+
+
+    #currently without any intake from data
+
+    #input value
+    if  <
+
 
 def main():
     print("I want a double cheeseburger and hold the lettuce, don't be frontin' son, no seeds on the bun.")
---
+++ OrderQueue.py
@@ -0,0 +1,13 @@
+#Order Station, this takes a while because you can't understand a god damn thing the person taking the order is saying
+queueSize = 0
+
+
+
+
+def add_to_queue():
+    queueSize += 1
+
+
+
+def get_queue_size():
+    return queueSize
