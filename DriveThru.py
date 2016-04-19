2e1957cb6fac43b9e3b0622bdccf6c2119d5d6dd
DRaines234 <DRaines574@gmail.com>
2016-04-18 23:35:42 -0400

    Started work on the time object like in ssq3 code, fixed a few bugs with method calls.

--- DriveThru.py
+++ DriveThru.py
@@ -3,18 +3,41 @@ __author__ = 'Derek and Spencer'
 import OrderQueue
 import PaymentWindow
 import PickupWindow
-import rngs
+import rvgs
 
 order = OrderQueue()
 payment = PaymentWindow()
 pickup = PickupWindow()
+
+def get_arrival():
+    get_arrival().arrival_time += rvgs.exponential(2.0) #adjusting this number will adjust our inter-arrival time
+    return get_arrival().arrival_time # arrival time is one of those function variable things that I cant remember the name of
+
+class time_structure:
+    def __init__(self):
+        self.arrival = -1                # next arrival time
+        self.orderCompletion = -1        # next order completion time
+        self.paymentCompletion = -1      # next payment completion time
+        self.pickupCopmletion = -1       # next pickup completion time
+        self.current = -1                # current time
+
 
 def run_sim(orderQueueSize, payQueueSize, pickupQueueSize, iterations):
     order.set_max = orderQueueSize
     payment.set_max = payQueueSize
     pickup.set_max = pickupQueueSize
     STOP = iterations
-    t = 0 # initialize the event clock to time 0
+    infinity = STOP * 1000
+
+    get_arrival().arrival_time = 0 # initialize arrival time to 0
+
+    t = time_structure()
+    t.current = 0 # set the simulation clock to 0
+    t.arrival = get_arrival() # get our first arrival time
+    # set completions to infinity to show we don't have any completions yet.
+    t.orderCompletion = infinity
+    t.paymentCompletion = infinity
+    t.pickupCopmletion = infinity
 
     #currently without any intake from data
 
